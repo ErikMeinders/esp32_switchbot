b9092e31c3fde0290c131e6c1a2f9397d2075cc7
Erik Meinders <erik@easytocloud.com>
2024-08-09 17:41:47 +0900

    fix: improved CICD pipeline for versioning

--- cicd/bump-version.sh
+++ cicd/bump-version.sh
@@ -1,12 +1,18 @@
 #
 # change version in library.json to be in sync with the tag on the repo
-# this version of the library file is not pushed!
+#
+# library.json is used in platformio packaging.
+# this script is part of the github action to update platformIO package
+#
 
-v=$(git tag |tail -1| cut -c2- )
-echo Tag vesion is v$v
+# (C) Erik Meinders
 
-jq --arg new_version "$v" '.version = $new_version' library.json > tmp.$$.json && mv tmp.$$.json library.json
+# assuming tag name is like v9.8.1 - remove the 'v' from the tag
+vless=$(git describe --tags --abbrev=0| tr -d 'v' )
+echo Repo Tag vesion is ${vless}
+
+jq --arg new_version "${vless}" '.version = $new_version' library.json > tmp.$$.json && mv tmp.$$.json library.json
 
 # Display version key of library.json
 
-echo Library version $(jq .version library.json)
+echo New Library version $(jq .version library.json)
